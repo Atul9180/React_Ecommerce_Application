37c8d529b67d8969e023d801b69efe98496d2fdd
Atul <patelatulpatel@gmail.com>
2023-10-03 21:10:06 +0530

    added css

--- client/src/components/TableReusableComponent.js
+++ client/src/components/TableReusableComponent.js
@@ -4,10 +4,19 @@ import "./TableReusableComponent.css";
 const TableReusableComponent = ({ item, onDelete }) => {
   return (
     <div className="tables">
-      <span>OrderId: {item.uniqueOrderId} ,</span>
-      <span>ItemPrice: {item.dishPrice}</span>
-      <span>Dish_Name: {item.dishName}</span>
-      <span>Table_Number: {item.tableNumber}</span>
+      <span>
+        OrderId:
+        <span> {item.uniqueOrderId} ,</span>
+      </span>
+      <span>
+        ItemPrice: <span>{item.dishPrice} ,</span>
+      </span>
+      <span>
+        Dish_Name: <span>{item.dishName} ,</span>
+      </span>
+      <span>
+        Table_Number: <span>{item.tableNumber}</span>
+      </span>
       <button onClick={() => onDelete(item)}>Delete</button>
     </div>
   );
